99a41779da3304d9394c535985f0b291dc538a14
edo92 <edoter92@gmail.com>
2024-07-29 08:04:46 -0700

    update hero component

--- apps/client/src/app/components/Hero/Hero.tsx
+++ apps/client/src/app/components/Hero/Hero.tsx
@@ -1,5 +1,7 @@
 import { Fragment } from 'react';
+import Link from 'next/link';
 import { cn } from '@client/lib/cn';
+import { Typography } from '@client/components/Typography';
 
 import { links } from './data';
 import { ABOUT_TEXT, NAME, HERO_TITLE } from './constants';
@@ -10,13 +12,17 @@ export const PageHero: React.FC = () => {
          <div className="flex w-full flex-col items-center justify-center p-9">
             <div className="mt-5 w-5/6 px-3 py-px">
                <div className="flex flex-col items-start px-12 pt-8">
-                  <div className="text-dark-300 dark:text-light-400 text-base max-md:max-w-full">
+                  <Typography variant="p" className="text-base max-md:max-w-full">
                      {HERO_TITLE}
-                  </div>
-                  <div className="text-gradient-100 text-8xl font-bold max-md:max-w-full">{NAME}</div>
-                  <div className="text-dark-300 dark:text-light-400 text-sm max-md:max-w-full">
+                  </Typography>
+
+                  <Typography variant="h1" className="text-gradient-100 text-8xl font-bold max-md:max-w-full">
+                     {NAME}
+                  </Typography>
+
+                  <Typography variant="p" className="text-sm max-md:max-w-full">
                      {ABOUT_TEXT}
-                  </div>
+                  </Typography>
                </div>
             </div>
          </div>
@@ -27,15 +33,22 @@ export const PageHero: React.FC = () => {
                   <div
                      key={index}
                      className={cn(
-                        'bg-gradient-to-tr',
                         item.style,
-                        'flex flex-col justify-center whitespace-nowrap rounded-sm p-0.5 text-center text-lg font-light'
+                        'bg-gradient-to-tr',
+                        'flex cursor-pointer flex-col justify-center whitespace-nowrap rounded p-0.5 text-center text-lg font-light'
                      )}
                   >
-                     <div className="bg-light-100 dark:bg-dark-300 dark:text-light-400 text-dark-300 flex items-center justify-center gap-2.5 px-3 py-2">
-                        <div>{item.name}</div>
+                     <Link
+                        target="_blank"
+                        href={item.link}
+                        className="bg-light-100 dark:bg-dark-300 flex flex-row items-center justify-center gap-2.5 rounded px-3 py-2"
+                     >
+                        <Typography variant="p" className="flex items-center justify-center">
+                           {item.name}
+                        </Typography>
+
                         <item.Icon className="dark:text-light-400 text-dark-100/85 aspect-square w-5 shrink-0" />
-                     </div>
+                     </Link>
                   </div>
                ))}
             </div>
