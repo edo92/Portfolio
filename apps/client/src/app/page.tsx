d61b98a4c63c1439037221ced78f3222ab1db0fe
edo92 <edoter92@gmail.com>
2024-07-29 02:04:38 -0700

    setup experaince timeline component

--- apps/client/src/app/page.tsx
+++ apps/client/src/app/page.tsx
@@ -1,11 +1,13 @@
 import { PageHero } from './components/Hero';
 import { LinkButtons } from './components/Buttons/Buttons';
+import { ExperianceTimeline } from './components/Experiance';
 
 export default function Index() {
    return (
-      <div className="dark:bg-dark-300 bg-light-100 h-screen w-full px-5">
+      <div className="dark:bg-dark-300 bg-light-100 size-full px-5">
          <PageHero />
          <LinkButtons />
+         <ExperianceTimeline />
       </div>
    );
 }
