d5de5fd3e20276d15150b9dd4fab8e0b400348bf
edo92 <edoter92@gmail.com>
2024-07-29 08:14:24 -0700

    update hearder component

--- apps/client/src/app/components/Header/Header.tsx
+++ apps/client/src/app/components/Header/Header.tsx
@@ -1,14 +1,39 @@
+import Link from 'next/link';
 import { DarkModeToggle } from './Toggle';
+import { Typography } from '@client/components/Typography';
 
 export const Header: React.FC = () => (
-   <div className="dark:bg-dark-300 text-dark-300 dark:text-light-400 bg-light-100 border-b-0.5 border-dark-100/10 flex w-full items-center justify-center whitespace-nowrap px-16 py-7 max-md:max-w-full max-md:px-5">
+   <div className="dark:bg-dark-300 bg-light-100 border-b-0.5 border-dark-100/10 flex w-full items-center justify-center whitespace-nowrap px-16 py-7 max-md:max-w-full max-md:px-5">
       <div className="flex w-full justify-between gap-2.5 max-md:max-w-full max-md:flex-wrap">
          <div className="text-lg">DR</div>
 
-         <div className="flex items-center justify-center gap-5 self-start px-1 text-sm">
-            <div>Projects</div>
-            <div>Resume</div>
-            <div>Contact</div>
+         <div className="flex items-center justify-center gap-5 self-start px-1">
+            <Link href="/projects">
+               <Typography
+                  variant="span"
+                  className="hover:text-primary-300 dark:hover:text-primary-100 text-sm"
+               >
+                  Projects
+               </Typography>
+            </Link>
+
+            <Link href="/resume">
+               <Typography
+                  variant="span"
+                  className="hover:text-primary-300 dark:hover:text-primary-100 text-sm"
+               >
+                  Resume
+               </Typography>
+            </Link>
+
+            <Link href="/contact">
+               <Typography
+                  variant="span"
+                  className="hover:text-primary-300 dark:hover:text-primary-100 text-sm"
+               >
+                  Contact
+               </Typography>
+            </Link>
 
             <div className="cursor-pointer">
                <DarkModeToggle />
