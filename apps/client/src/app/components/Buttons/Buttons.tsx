1e38770395e2858905a4263535d7de34144ff5fd
edo92 <edoter92@gmail.com>
2024-07-29 01:49:38 -0700

    update link buttons components

--- apps/client/src/app/components/Buttons/Buttons.tsx
+++ apps/client/src/app/components/Buttons/Buttons.tsx
@@ -1,5 +1,5 @@
-import { Icons } from '@client/components/Icons';
 import { cn } from '@client/lib/cn';
+import { Icons } from '@client/components/Icons';
 
 export const links = [
   {
@@ -10,17 +10,17 @@ export const links = [
   {
     name: 'LinkedIn',
     Icon: Icons.LinkedIn,
-    style: 'from-gradient-19 to-gradient-20',
+    style: 'from-gradient-18 to-gradient-19',
   },
   {
     name: 'GitHub',
     Icon: Icons.GitHub,
-    style: 'from-gradient-21 to-gradient-22',
+    style: 'from-gradient-20 to-gradient-21',
   },
   {
     name: 'LinkedIn',
     Icon: Icons.LinkedIn,
-    style: 'from-gradient-23 to-gradient-24',
+    style: 'from-gradient-22 to-gradient-23',
   },
 ];
 
@@ -33,10 +33,10 @@ export const LinkButtons: React.FC = () => (
           className={cn(
             'bg-gradient-to-tr',
             item.style,
-            'flex flex-col justify-center whitespace-nowrap rounded-sm p-1 text-center text-lg font-light text-neutral-900'
+            'flex flex-col justify-center whitespace-nowrap rounded-sm p-0.5 text-center text-lg font-light'
           )}
         >
-          <div className="bg-light-100 flex items-center justify-center gap-2.5 px-3 py-2">
+          <div className="bg-light-100 dark:bg-dark-300 dark:text-light-400 text-dark-300 flex items-center justify-center gap-2.5 px-3 py-2">
             <div>{item.name}</div>
             <item.Icon className="aspect-square w-5 shrink-0" />
           </div>
