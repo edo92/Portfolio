e6dc9e95153a379577946fb57fd1988cddf1e259
edo92 <edoter92@gmail.com>
2024-07-29 07:47:41 -0700

    update projects component

--- apps/client/src/app/components/Projects/Project.tsx
+++ apps/client/src/app/components/Projects/Project.tsx
@@ -1,3 +1,5 @@
+import Link from 'next/link';
+import Image from 'next/image';
 import { Icons } from '@client/components/Icons';
 import { Typography } from '@client/components/Typography';
 
@@ -6,28 +8,38 @@ interface ProjectProps {
    description: string;
    stack: string[];
    link: string;
+   imageUrl: string;
 }
 
-export const Project: React.FC<ProjectProps> = ({ name, description, stack, link }) => {
+export const Project: React.FC<ProjectProps> = ({ name, description, stack, imageUrl, link }) => {
    return (
       <div className="ml-5 flex w-2/6 flex-col max-md:ml-0 max-md:w-full">
          <div className="flex grow flex-col gap-3 text-sm font-light">
-            <img
-               loading="lazy"
-               srcSet="https://cdn.builder.io/api/v1/image/assets/TEMP/f2e97ab2969ac6aac36b09f470d6fece325a7c2323fcdc929d0eb7e72f629629?apiKey=e3660d42c58b4c259ebe598b9e99176a&width=100 100w, https://cdn.builder.io/api/v1/image/assets/TEMP/f2e97ab2969ac6aac36b09f470d6fece325a7c2323fcdc929d0eb7e72f629629?apiKey=e3660d42c58b4c259ebe598b9e99176a&width=200 200w, https://cdn.builder.io/api/v1/image/assets/TEMP/f2e97ab2969ac6aac36b09f470d6fece325a7c2323fcdc929d0eb7e72f629629?apiKey=e3660d42c58b4c259ebe598b9e99176a&width=400 400w, https://cdn.builder.io/api/v1/image/assets/TEMP/f2e97ab2969ac6aac36b09f470d6fece325a7c2323fcdc929d0eb7e72f629629?apiKey=e3660d42c58b4c259ebe598b9e99176a&width=800 800w, https://cdn.builder.io/api/v1/image/assets/TEMP/f2e97ab2969ac6aac36b09f470d6fece325a7c2323fcdc929d0eb7e72f629629?apiKey=e3660d42c58b4c259ebe598b9e99176a&width=1200 1200w, https://cdn.builder.io/api/v1/image/assets/TEMP/f2e97ab2969ac6aac36b09f470d6fece325a7c2323fcdc929d0eb7e72f629629?apiKey=e3660d42c58b4c259ebe598b9e99176a&width=1600 1600w, https://cdn.builder.io/api/v1/image/assets/TEMP/f2e97ab2969ac6aac36b09f470d6fece325a7c2323fcdc929d0eb7e72f629629?apiKey=e3660d42c58b4c259ebe598b9e99176a&width=2000 2000w"
-               className="aspect-[1.45] w-full rounded"
-            />
+            <div className="border-light-400 rounded border p-1">
+               <Image
+                  src={imageUrl}
+                  className="aspect-[1.45] w-full rounded"
+                  alt="Project image"
+                  width={2000}
+                  height={1380}
+               />
+            </div>
 
-            <Typography variant="h3">{name}</Typography>
-            <Typography variant="p">{description}</Typography>
-            <Typography variant="p" className="text-primary-300 dark:text-primary-100 py-3 leading-6">
-               {stack.join(' - ')}
-            </Typography>
+            <div className="ml-1 flex w-full flex-col gap-3">
+               <Typography variant="h3">{name}</Typography>
+               <Typography variant="p">{description}</Typography>
+               <Typography variant="p" className="text-primary-300 dark:text-primary-100 py-3 leading-6">
+                  {stack.join(' - ')}
+               </Typography>
 
-            <div className="from-gradient-20 to-gradient-21 w-full rounded bg-gradient-to-tr p-0.5">
-               <div className="bg-light-100 flex flex-row items-center justify-center gap-3 self-start px-10 py-3">
-                  <Icons.Link className="text-dark-300/85 dark:text-light-400 size-[16px] shrink-0" />
-                  <Typography variant="p">Live demo</Typography>
+               <div className="from-gradient-20 to-gradient-21 hover:from-gradient-19 hover:to-gradient-20 cursor-pointer rounded bg-gradient-to-tr p-0.5 transition-all duration-300 ease-in-out">
+                  <Link
+                     href={link}
+                     className="bg-light-100 flex flex-row items-center justify-center gap-3 py-3"
+                  >
+                     <Icons.Link className="text-dark-300/85 dark:text-light-400 size-[16px]" />
+                     <Typography variant="p">Live demo</Typography>
+                  </Link>
                </div>
             </div>
          </div>
--- apps/client/src/app/components/Projects/data.ts
+++ apps/client/src/app/components/Projects/data.ts
@@ -4,18 +4,24 @@ export const projects = [
       description: 'Proxy provider website including authentication, dashboard and dynamic features',
       stack: ['React', 'Bootstrap', 'Styled Components'],
       link: 'https://www.google.com',
+      imageUrl:
+         'https://cdn.builder.io/api/v1/image/assets/TEMP/f2e97ab2969ac6aac36b09f470d6fece325a7c2323fcdc929d0eb7e72f629629?apiKey=e3660d42c58b4c259ebe598b9e99176a&width=2000',
    },
    {
       name: 'Project One',
       description: 'Proxy provider website including authentication, dashboard and dynamic features',
       stack: ['React', 'Bootstrap', 'Styled Components'],
       link: 'https://www.google.com',
+      imageUrl:
+         'https://cdn.builder.io/api/v1/image/assets/TEMP/f2e97ab2969ac6aac36b09f470d6fece325a7c2323fcdc929d0eb7e72f629629?apiKey=e3660d42c58b4c259ebe598b9e99176a&width=2000',
    },
    {
       name: 'Project One',
       description: 'Proxy provider website including authentication, dashboard and dynamic features',
       stack: ['React', 'Bootstrap', 'Styled Components'],
       link: 'https://www.google.com',
+      imageUrl:
+         'https://cdn.builder.io/api/v1/image/assets/TEMP/f2e97ab2969ac6aac36b09f470d6fece325a7c2323fcdc929d0eb7e72f629629?apiKey=e3660d42c58b4c259ebe598b9e99176a&width=2000',
    },
 
    {
@@ -23,17 +29,23 @@ export const projects = [
       description: 'Proxy provider website including authentication, dashboard and dynamic features',
       stack: ['React', 'Bootstrap', 'Styled Components'],
       link: 'https://www.google.com',
+      imageUrl:
+         'https://cdn.builder.io/api/v1/image/assets/TEMP/f2e97ab2969ac6aac36b09f470d6fece325a7c2323fcdc929d0eb7e72f629629?apiKey=e3660d42c58b4c259ebe598b9e99176a&width=2000',
    },
    {
       name: 'Project One',
       description: 'Proxy provider website including authentication, dashboard and dynamic features',
       stack: ['React', 'Bootstrap', 'Styled Components'],
       link: 'https://www.google.com',
+      imageUrl:
+         'https://cdn.builder.io/api/v1/image/assets/TEMP/f2e97ab2969ac6aac36b09f470d6fece325a7c2323fcdc929d0eb7e72f629629?apiKey=e3660d42c58b4c259ebe598b9e99176a&width=2000',
    },
    {
       name: 'Project One',
       description: 'Proxy provider website including authentication, dashboard and dynamic features',
       stack: ['React', 'Bootstrap', 'Styled Components'],
       link: 'https://www.google.com',
+      imageUrl:
+         'https://cdn.builder.io/api/v1/image/assets/TEMP/f2e97ab2969ac6aac36b09f470d6fece325a7c2323fcdc929d0eb7e72f629629?apiKey=e3660d42c58b4c259ebe598b9e99176a&width=2000',
    },
 ];
