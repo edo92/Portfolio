655ba70131255bd6ba0020fe7af9220118d7cf18
edo92 <edoter92@gmail.com>
2024-07-29 06:50:00 -0700

    update experiance component

--- apps/client/src/app/components/Experience/Experience.tsx
+++ apps/client/src/app/components/Experience/Experience.tsx
@@ -1,10 +1,13 @@
+import { Typography } from '@client/components/Typography';
 import { Timeline } from './Timeline';
 
 export const Experience: React.FC = () => (
    <div className="flex flex-col justify-center px-16 py-6 pt-48 max-md:max-w-full max-md:px-5">
       <div className="p-3 max-md:max-w-full">
          <div className="flex w-full flex-row justify-start gap-2">
-            <h1 className="text-dark-300 dark:text-light-400 text-center text-4xl font-bold">Experience</h1>
+            <Typography variant="h1" className="font-bold">
+               Experience
+            </Typography>
          </div>
          <Timeline />
       </div>
