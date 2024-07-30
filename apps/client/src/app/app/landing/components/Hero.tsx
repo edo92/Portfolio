43d0eedfe62b162b081ac67a6c9c4116ddc10027
edo92 <edoter92@gmail.com>
2024-07-30 05:02:51 -0700

    update hero component

--- apps/client/src/app/app/landing/components/Hero.tsx
+++ apps/client/src/app/app/landing/components/Hero.tsx
@@ -3,19 +3,25 @@ import { ABOUT_TEXT, HERO_TITLE, NAME } from './constants';
 
 export const Hero: React.FC = () => {
    return (
-      <div className="flex w-full flex-col items-center justify-center">
-         <div className="flex flex-col items-start px-12 pt-8 leading-[1.3] md:leading-normal">
-            <Paragraph variant="p" size="base" className="max-md:max-w-full">
-               {HERO_TITLE}
-            </Paragraph>
+      <div className="flex w-full flex-col items-center justify-center p-0 xl:pl-12">
+         <div className="flex w-full flex-col items-center justify-center p-0 md:pl-7 lg:pl-9">
+            <div className="flex w-7/12 flex-col items-start p-0 md:p-3">
+               <Paragraph variant="p" size="base" className="text-center">
+                  {HERO_TITLE}
+               </Paragraph>
 
-            <Heading variant="h1" size="8xl" font="bold" className="text-gradient-100 max-md:max-w-full">
-               {NAME}
-            </Heading>
+               <Heading
+                  variant="h1"
+                  font="bold"
+                  className="text-gradient-100 text-7xl md:truncate lg:text-8xl"
+               >
+                  {NAME}
+               </Heading>
 
-            <Paragraph variant="p" size="sm" className="max-md:max-w-full">
-               {ABOUT_TEXT}
-            </Paragraph>
+               <Paragraph variant="p" size="sm" className="relative inline-block pt-3 leading-[1.7]">
+                  {ABOUT_TEXT}
+               </Paragraph>
+            </div>
          </div>
       </div>
    );
