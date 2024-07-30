0a9bb4dc338bd0a8f8885499cfb8967948463ab4
edo92 <edoter92@gmail.com>
2024-07-30 06:16:20 -0700

    update tailwind darkmode config

--- apps/client/src/app/providers.tsx
+++ apps/client/src/app/providers.tsx
@@ -1,5 +1,5 @@
 import { ThemeProvider } from 'next-themes';
 
 export const Providers: React.FC<React.PropsWithChildren> = ({ children }) => (
-   <ThemeProvider attribute="class" defaultTheme='dark'>{children}</ThemeProvider>
+   <ThemeProvider attribute="class">{children}</ThemeProvider>
 );
--- apps/client/tailwind.config.js
+++ apps/client/tailwind.config.js
@@ -18,6 +18,8 @@ module.exports = {
       ...createGlobPatternsForDependencies(__dirname),
    ],
 
+   darkMode: 'class',
+
    theme: {
       extend: {
          colors: {
