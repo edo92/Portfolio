078105db71ee5086ed2f39c1ed1102a98510b07f
edo92 <edoter92@gmail.com>
2024-07-30 03:32:08 -0700

    update header theme switch

--- apps/client/src/app/components/Header.tsx
+++ apps/client/src/app/components/Header.tsx
@@ -1,6 +1,7 @@
 import dynamic from 'next/dynamic';
 const ThemeSwitch = dynamic(() => import('./ThemeSwitch').then((c) => c.ThemeSwitch), {
    ssr: false,
+   loading: () => <div>Loading...</div>,
 });
 
 export const Header: React.FC = () => {
--- apps/client/src/app/components/ThemeSwitch.tsx
+++ apps/client/src/app/components/ThemeSwitch.tsx
@@ -4,9 +4,10 @@ import { useTheme } from 'next-themes';
 
 export const ThemeSwitch: React.FC = () => {
    const { resolvedTheme, setTheme } = useTheme();
+
    return (
       <button
-         className="mt-16 rounded-md bg-black px-4 py-2 font-semibold text-white dark:bg-white dark:text-black"
+         className="rounded-md bg-black px-4 py-2 font-semibold text-white dark:bg-white dark:text-black"
          onClick={() => {
             setTheme(resolvedTheme === 'light' ? 'dark' : 'light');
          }}
