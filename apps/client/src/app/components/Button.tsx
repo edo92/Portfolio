d99d3a215bb589e8692b00b6700462b770f19242
edo92 <edoter92@gmail.com>
2024-07-30 05:14:59 -0700

    setup button gradiant

---
+++ apps/client/src/app/components/Button.tsx
@@ -0,0 +1,18 @@
+import { cn } from '@client/lib/cn';
+
+interface ButtonProps extends React.PropsWithChildren {
+   className?: string;
+}
+
+export const ButtonGradiant: React.FC<ButtonProps> = ({ children, className }) => (
+   <button className={cn('from-gradient-17 to-gradient-18 min-w-48 rounded bg-gradient-to-tr p-0.5')}>
+      <div
+         className={cn(
+            'bg-light-100 dark:bg-dark-300 flex w-full flex-row items-center justify-center rounded px-5 py-2',
+            className
+         )}
+      >
+         {children}
+      </div>
+   </button>
+);
