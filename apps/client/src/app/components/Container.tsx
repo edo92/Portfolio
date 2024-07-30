8b2a9fa0f196ca625e4d0c87bde8f22a4d14ebca
edo92 <edoter92@gmail.com>
2024-07-30 05:02:31 -0700

    update container component

--- apps/client/src/app/components/Container.tsx
+++ apps/client/src/app/components/Container.tsx
@@ -1,6 +1,6 @@
 export const Container: React.FC<React.PropsWithChildren> = ({ children }) => (
    <div className="relative flex h-screen w-screen">
-      <div className="flex flex-col items-center p-3">
+      <div className="flex w-full flex-col items-center p-3">
          <>{children}</>
       </div>
    </div>
