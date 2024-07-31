b275afa8461afa7cf9840dca905f2965715a7a69
edo92 <edoter92@gmail.com>
2024-07-30 20:24:32 -0700

    separate profile links as component

--- apps/client/src/app/app/landing/page.tsx
+++ apps/client/src/app/app/landing/page.tsx
@@ -1,10 +1,12 @@
 import { Container } from '@client/app/components/Container';
 import { Hero } from './components/Hero';
+import { Profiles } from './components/Profiles';
 
 export default function LandingPage() {
    return (
       <Container>
          <Hero />
+         <Profiles />
       </Container>
    );
 }
