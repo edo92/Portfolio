c67e7adbc0719d7ea7a5741767784c6df53c8bdc
edo92 <edoter92@gmail.com>
2024-07-30 22:39:24 -0700

    update hero component

--- apps/client/src/app/app/landing/components/Hero.tsx
+++ apps/client/src/app/app/landing/components/Hero.tsx
@@ -4,22 +4,27 @@ import * as intro from '@client/app/content/intro';
 
 export const Hero: React.FC = () => {
    return (
-      <div className="flex w-full flex-col items-center justify-center p-0 pt-9 md:pt-12 xl:pl-12">
+      <div className="flex w-full flex-col items-center justify-center p-0 pt-9 md:pt-20 xl:pl-12">
          <div className="flex w-full flex-col items-center justify-center p-0 xl:pl-9">
             <HeroContainer className="flex flex-col items-start gap-3 p-0 leading-tight md:gap-0 md:p-3 md:leading-normal">
-               <Paragraph variant="p" size="base" className="pl-8 md:pl-0">
+               <Paragraph variant="p" size="base" font="light" className="pl-8 md:pl-0">
                   {intro.HERO_TITLE}
                </Paragraph>
 
                <Heading
                   variant="h1"
                   font="bold"
-                  className="text-gradient-100 pl-8 text-7xl md:truncate md:pl-0 lg:text-8xl"
+                  className="text-gradient-500 pl-8 text-7xl md:truncate md:pl-0 lg:text-8xl"
                >
                   {intro.NAME}
                </Heading>
 
-               <Paragraph variant="p" size="sm" className="relative inline-block pt-3 leading-[1.7]">
+               <Paragraph
+                  variant="p"
+                  size="md"
+                  font="light"
+                  className="relative inline-block pt-3 leading-[1.7]"
+               >
                   {intro.ABOUT_TEXT}
                </Paragraph>
             </HeroContainer>
