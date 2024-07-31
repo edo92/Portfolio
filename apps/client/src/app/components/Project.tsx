17f6469d2dfac4fde9b618926eb8739cac2a16bb
edo92 <edoter92@gmail.com>
2024-07-31 02:57:06 -0700

    update projects | components

--- apps/client/src/app/components/Project.tsx
+++ apps/client/src/app/components/Project.tsx
@@ -14,7 +14,7 @@ interface ProjectProps {
 export const Project: React.FC<ProjectProps> = ({ name, description, stack, imageUrl, link }) => (
    <div className="flex w-2/6 flex-col max-md:w-full">
       <div className="flex grow flex-col gap-3 text-sm font-light">
-         <div className="border-light-400 rounded border p-1 shadow-sm">
+         <div className="border-light-400 dark:border-light-200/70 rounded border p-[3px] shadow-sm">
             <Image
                src={imageUrl}
                className="aspect-[1.45] w-full rounded"
@@ -29,13 +29,17 @@ export const Project: React.FC<ProjectProps> = ({ name, description, stack, imag
                {name}
             </Heading>
 
-            <Paragraph variant="p" size="base">
-               {description}
-            </Paragraph>
+            <div className="h-20 w-full">
+               <Paragraph variant="p" size="base" font="light">
+                  {description}
+               </Paragraph>
+            </div>
 
-            <Paragraph variant="p" className="text-primary-200 dark:text-primary-100 py-2">
-               {stack.join(' - ')}
-            </Paragraph>
+            <div className="h-14 w-full">
+               <Paragraph variant="p" className="text-primary-200 dark:text-primary-100 py-2">
+                  {stack.join(' - ')}
+               </Paragraph>
+            </div>
 
             <div className="from-gradient-20 to-gradient-21 hover:from-gradient-19 hover:to-gradient-20 cursor-pointer rounded bg-gradient-to-tr p-0.5 transition-all duration-300 ease-in-out">
                <Link
