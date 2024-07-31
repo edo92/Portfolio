600c5921d38f8a1de7aa3af410c2c1e0989a96c3
edo92 <edoter92@gmail.com>
2024-07-30 21:12:43 -0700

    update profiles component

--- apps/client/src/app/app/landing/components/Profiles.tsx
+++ apps/client/src/app/app/landing/components/Profiles.tsx
@@ -3,17 +3,18 @@ import { Button } from '@client/components/Button';
 import { Paragraph } from '@client/components/Typography';
 import { links } from '@client/app/content/profiles';
 
-export const Profiles = () => (
-   <div className="mt-2 flex w-full scale-75 self-stretch pt-12 md:scale-100 md:px-0 md:pt-28">
+export const Profiles: React.FC = () => (
+   <div className="mt-5 flex w-full self-stretch pt-12 md:mt-2 md:px-0 md:pt-28">
       <div className="flex w-full flex-wrap items-center justify-center gap-4 px-3">
          {links.map((item, index) => (
             <div
                key={index}
                className={cn(
-                  'flex cursor-pointer flex-col justify-center whitespace-nowrap rounded-md  text-center text-lg font-light'
+                  item.style,
+                  'flex cursor-pointer flex-col justify-center whitespace-nowrap rounded-md p-0.5 text-center text-lg font-light'
                )}
             >
-               <Button variant="ghost" className="from-gradient-17 to-gradient-18 bg-gradient-to-tr p-0.5">
+               <Button variant="ghost">
                   <div className="bg-light-100 dark:bg-dark-300 flex w-36 flex-row items-center justify-center gap-3 rounded py-3">
                      <Paragraph variant="p" size="base" className="flex items-center justify-center">
                         {item.name}
