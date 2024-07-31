9536cefda8c23a13b4d7ad51befe21e6ff59e19b
edo92 <edoter92@gmail.com>
2024-07-31 05:32:16 -0700

    update landing page

--- apps/client/src/app/app/landing/page.tsx
+++ apps/client/src/app/app/landing/page.tsx
@@ -2,13 +2,18 @@ import { Container } from '@client/app/components/Container';
 import { Hero } from './components/Hero';
 import { Profiles } from './components/Profiles';
 import { Projects } from './components/Projects';
+import { Timeline } from './components/Timeline';
+import { Section } from './components/Section';
 
 export default function LandingPage() {
    return (
       <Container>
          <Hero />
          <Profiles />
-         <Projects />
+         <Section>
+            <Timeline />
+            <Projects />
+         </Section>
       </Container>
    );
 }
