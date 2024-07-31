f988392a440294d3b5406d292fa6e2ed9fa83ac0
edo92 <edoter92@gmail.com>
2024-07-30 19:47:20 -0700

    update hero component

--- apps/client/src/app/app/landing/components/Hero.tsx
+++ apps/client/src/app/app/landing/components/Hero.tsx
@@ -1,7 +1,8 @@
 import { cn } from '@client/lib/cn';
 import { Icons } from '@client/components/Icons';
-import { ButtonGradiant } from '@client/app/components/Button';
+import { Button } from '@client/components/Button';
 import { Heading, Paragraph } from '@client/components/Typography';
+import { HeroContainer } from '@client/app/app/landing/components/HeroContainer';
 import { ABOUT_TEXT, HERO_TITLE, NAME } from './constants';
 
 export const links = [
@@ -35,7 +36,7 @@ export const Hero: React.FC = () => {
    return (
       <div className="flex w-full flex-col items-center justify-center p-0 pt-9 md:pt-12 xl:pl-12">
          <div className="flex w-full flex-col items-center justify-center p-0 lg:pl-9">
-            <div className="flex w-7/12 flex-col items-start gap-3 p-0 leading-tight md:gap-0 md:p-3 md:leading-normal">
+            <HeroContainer className="flex flex-col items-start gap-3 p-0 leading-tight md:gap-0 md:p-3 md:leading-normal">
                <Paragraph variant="p" size="base" className="text-center">
                   {HERO_TITLE}
                </Paragraph>
@@ -51,7 +52,7 @@ export const Hero: React.FC = () => {
                <Paragraph variant="p" size="sm" className="relative inline-block pt-3 leading-[1.7]">
                   {ABOUT_TEXT}
                </Paragraph>
-            </div>
+            </HeroContainer>
          </div>
 
          <div className="mt-2 flex w-full scale-75 self-stretch pt-12 md:scale-100 md:px-0 md:pt-28">
@@ -60,16 +61,20 @@ export const Hero: React.FC = () => {
                   <div
                      key={index}
                      className={cn(
-                        item.style,
-                        'bg-gradient-to-tr',
-                        'flex cursor-pointer flex-col justify-center whitespace-nowrap rounded p-0.5 text-center text-lg font-light'
+                        'flex cursor-pointer flex-col justify-center whitespace-nowrap rounded-md  text-center text-lg font-light'
                      )}
                   >
-                     <ButtonGradiant>
-                        <Paragraph variant="p" className="flex items-center justify-center">
-                           {item.name}
-                        </Paragraph>
-                     </ButtonGradiant>
+                     <Button
+                        variant="ghost"
+                        className="from-gradient-17 to-gradient-18 bg-gradient-to-tr p-0.5"
+                     >
+                        <div className="bg-light-100 dark:bg-dark-300 flex w-48 flex-row items-center justify-center gap-3 rounded py-3">
+                           <Paragraph variant="p" size="base" className="flex items-center justify-center">
+                              {item.name}
+                           </Paragraph>
+                           <item.Icon className="dark:text-light-400 text-dark-100/85 aspect-square w-[18px] shrink-0" />
+                        </div>
+                     </Button>
                   </div>
                ))}
             </div>
