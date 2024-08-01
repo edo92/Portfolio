5ac27830eeac26fc55e7b0daaea7b30c8ff5b323
edo92 <edoter92@gmail.com>
2024-08-01 02:27:24 -0700

    update container component

--- apps/client/src/app/components/Container.tsx
+++ apps/client/src/app/components/Container.tsx
@@ -5,8 +5,8 @@ interface ContainerProps extends React.PropsWithChildren {
 }
 
 export const Container: React.FC<ContainerProps> = ({ children, className }) => (
-   <div className={cn('relative flex h-screen w-screen py-32', className)}>
-      <div className="flex w-full flex-col items-center gap-32">
+   <div className={cn('relative mt-3 flex h-screen w-screen py-16 sm:py-32 md:mt-0 md:py-28', className)}>
+      <div className="flex w-full flex-col items-center gap-16 sm:gap-32 md:gap-28">
          <>{children}</>
       </div>
    </div>
