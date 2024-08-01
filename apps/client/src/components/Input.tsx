3050cd9b3871a24a6c71ad16c400566a7d56b2f8
edo92 <edoter92@gmail.com>
2024-08-01 06:19:26 -0700

    setup input component

---
+++ apps/client/src/components/Input.tsx
@@ -0,0 +1,24 @@
+import { cn } from '@client/lib/cn';
+
+export const Input: React.FC<React.InputHTMLAttributes<HTMLInputElement>> = ({ className, ...props }) => (
+   <input
+      className={cn('input input-bordered dark:bg-dark-300/80 bg-light-300/80 w-full', className)}
+      {...props}
+   />
+);
+
+export const TextArea: React.FC<React.TextareaHTMLAttributes<HTMLTextAreaElement>> = ({
+   className,
+   ...props
+}) => (
+   <textarea
+      className={cn('textarea textarea-bordered dark:bg-dark-300/80 bg-light-300/80 w-full', className)}
+      {...props}
+   />
+);
+
+export const Label: React.FC<React.LabelHTMLAttributes<HTMLLabelElement>> = ({ children, ...props }) => (
+   <label className="dark:text-light-400 text-dark-300 block" {...props}>
+      <>{children}</>
+   </label>
+);
