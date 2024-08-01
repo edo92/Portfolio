68154f310e10374fc5a81afd7b6a07d0ab9fd3bf
edo92 <edoter92@gmail.com>
2024-08-01 03:06:49 -0700

    reusable project component

--- apps/client/src/app/app/landing/components/Projects.tsx
+++ apps/client/src/app/app/landing/components/Projects.tsx
@@ -1,7 +1,11 @@
-import { Project } from '@client/app/components/Project';
+import Link from 'next/link';
 import { projects } from '@client/app/content/projects';
-import { Heading } from '@client/components/Typography';
 import { divideIntoChunks } from '@client/util/divideChunks';
+
+import { Icons } from '@client/components/Icons';
+import { Button } from '@client/components/Button';
+import { Project } from '@client/app/components/Project';
+import { Heading, Paragraph } from '@client/components/Typography';
 
 export const Projects: React.FC = () => {
    const projectList = divideIntoChunks(projects, 3);
@@ -16,10 +20,43 @@ export const Projects: React.FC = () => {
             </div>
 
             <div className="flex w-full flex-col items-center justify-center gap-20">
-               {projectList.map((chunk, index) => (
+               {projectList.map((list, index) => (
                   <div key={index} className="flex flex-row gap-20 max-md:flex-col md:gap-9">
-                     {chunk.map((project) => (
-                        <Project key={project.name} {...project} />
+                     {list.map(({ name, description, stack, link, imageUrl }) => (
+                        <Project key={name} imageUrl={imageUrl}>
+                           <div className="ml-1 flex w-full flex-col gap-3">
+                              <Heading variant="h3" size="xl">
+                                 {name}
+                              </Heading>
+
+                              <div className="h-20 w-full">
+                                 <Paragraph variant="p" size="base" font="light">
+                                    {description}
+                                 </Paragraph>
+                              </div>
+
+                              <div className="h-14 w-full">
+                                 <Paragraph
+                                    variant="p"
+                                    className="text-primary-200 dark:text-primary-100 py-2"
+                                 >
+                                    {stack.join(' - ')}
+                                 </Paragraph>
+                              </div>
+
+                              <div className="bg-gradient-100 flex w-full cursor-pointer flex-col justify-center whitespace-nowrap rounded-md p-0.5 text-center text-lg font-light">
+                                 <Button variant="ghost">
+                                    <Link
+                                       href={link}
+                                       className="bg-light-100 dark:bg-dark-300 flex w-full flex-row items-center justify-center gap-3 rounded py-3"
+                                    >
+                                       <Icons.Link className="dark:text-light-400 text-dark-100/85 aspect-square w-[16px] shrink-0" />
+                                       <Paragraph variant="p">Live demo</Paragraph>
+                                    </Link>
+                                 </Button>
+                              </div>
+                           </div>
+                        </Project>
                      ))}
                   </div>
                ))}
--- apps/client/src/app/components/Project.tsx
+++ apps/client/src/app/components/Project.tsx
@@ -1,18 +1,10 @@
-import Link from 'next/link';
 import Image from 'next/image';
-import { Icons } from '@client/components/Icons';
-import { Button } from '@client/components/Button';
-import { Paragraph, Heading } from '@client/components/Typography';
 
-interface ProjectProps {
-   name: string;
-   description: string;
-   stack: string[];
-   link: string;
+interface ProjectProps extends React.PropsWithChildren {
    imageUrl: string;
 }
 
-export const Project: React.FC<ProjectProps> = ({ name, description, stack, imageUrl, link }) => (
+export const Project: React.FC<ProjectProps> = ({ children, imageUrl }) => (
    <div className="flex w-2/6 flex-col max-md:w-full">
       <div className="flex grow flex-col gap-3 text-sm font-light">
          <div className="border-light-400 dark:border-light-200/70 rounded border p-[3px] shadow-sm">
@@ -24,36 +16,7 @@ export const Project: React.FC<ProjectProps> = ({ name, description, stack, imag
                height={1380}
             />
          </div>
-
-         <div className="ml-1 flex w-full flex-col gap-3">
-            <Heading variant="h3" size="xl">
-               {name}
-            </Heading>
-
-            <div className="h-20 w-full">
-               <Paragraph variant="p" size="base" font="light">
-                  {description}
-               </Paragraph>
-            </div>
-
-            <div className="h-14 w-full">
-               <Paragraph variant="p" className="text-primary-200 dark:text-primary-100 py-2">
-                  {stack.join(' - ')}
-               </Paragraph>
-            </div>
-
-            <div className="bg-gradient-100 flex w-full cursor-pointer flex-col justify-center whitespace-nowrap rounded-md p-0.5 text-center text-lg font-light">
-               <Button variant="ghost">
-                  <Link
-                     href={link}
-                     className="bg-light-100 dark:bg-dark-300 flex w-full flex-row items-center justify-center gap-3 rounded py-3"
-                  >
-                     <Icons.Link className="dark:text-light-400 text-dark-100/85 aspect-square w-[16px] shrink-0" />
-                     <Paragraph variant="p">Live demo</Paragraph>
-                  </Link>
-               </Button>
-            </div>
-         </div>
+         {children}
       </div>
    </div>
 );
