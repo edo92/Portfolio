33b1ed0992f492a6033d1ff8b171335caac926cc
edo92 <edoter92@gmail.com>
2024-08-01 03:38:44 -0700

    update section component

--- apps/client/src/app/components/Section.tsx
+++ apps/client/src/app/components/Section.tsx
@@ -1,5 +1,16 @@
-export const Section: React.FC<React.PropsWithChildren> = ({ children }) => (
-   <div className="dark:bg-dark-400 bg-light-100 flex w-full flex-col gap-16 px-0 pt-16 md:gap-28 md:px-14 md:pt-32">
+import { cn } from '@client/lib/cn';
+
+interface SectionProps extends React.PropsWithChildren {
+   className?: string;
+}
+
+export const Section: React.FC<SectionProps> = ({ children, className }) => (
+   <div
+      className={cn(
+         'dark:bg-dark-400 bg-light-100 flex w-full flex-col gap-16 px-0 pt-16 md:gap-28 md:px-14 md:pt-32',
+         className
+      )}
+   >
       {children}
    </div>
 );
