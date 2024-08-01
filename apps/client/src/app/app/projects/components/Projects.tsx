5ecdcc6170abc192e04b40635897e999658bdb2f
edo92 <edoter92@gmail.com>
2024-08-01 03:37:06 -0700

    projects page - update project component

--- apps/client/src/app/app/projects/components/Projects.tsx
+++ apps/client/src/app/app/projects/components/Projects.tsx
@@ -1,11 +1,51 @@
 import { Icons } from '@client/components/Icons';
+import { Button } from '@client/components/Button';
 import { Project } from '@client/app/components/Project';
 import { Heading, Paragraph } from '@client/components/Typography';
 
 import { projects } from '@client/app/content/projects';
 import { divideIntoChunks } from '@client/util/divideChunks';
 
-export const Projects = () => {
+const ProjectComponent: React.FC<{
+   name: string;
+   description: string;
+   stack: string[];
+   imageUrl: string;
+}> = ({ imageUrl, name, description, stack }) => (
+   <Project imageUrl={imageUrl}>
+      <div className="ml-1 flex w-full flex-col gap-3">
+         <div className="flex w-full flex-row justify-between gap-3">
+            <Heading variant="h3" size="xl">
+               {name}
+            </Heading>
+
+            <div className="flex flex-row gap-3 px-3">
+               <Button variant="ghost" className="group h-6 shadow-none">
+                  <Icons.GitHub className="dark:text-light-400 text-dark-300 transition-100 m-0 aspect-[1.05] size-[1.1rem] shrink-0 transition-all group-hover:scale-[110%]" />
+               </Button>
+
+               <Button variant="ghost" className="group h-6 shadow-none">
+                  <Icons.Link className="dark:text-light-400 text-dark-300 transition-100 m-0 aspect-[1.05] size-[1.1rem] shrink-0 transition-all group-hover:scale-[110%]" />
+               </Button>
+            </div>
+         </div>
+
+         <Paragraph variant="p" size="base" font="light" className="mt-2 leading-6 max-md:max-w-full">
+            {description}
+         </Paragraph>
+
+         <Paragraph
+            font="light"
+            variant="small"
+            className="text-primary-200 dark:text-primary-100 py-2 max-md:max-w-full"
+         >
+            {stack.join(' - ')}
+         </Paragraph>
+      </div>
+   </Project>
+);
+
+export const Projects: React.FC = () => {
    const projectList = divideIntoChunks(projects, 3);
 
    return (
@@ -18,38 +58,8 @@ export const Projects = () => {
             <div className="flex w-full flex-col items-center justify-center gap-20">
                {projectList.map((list, index) => (
                   <div key={index} className="flex flex-row gap-20 max-md:flex-col md:gap-9">
-                     {list.map(({ name, description, stack, imageUrl }) => (
-                        <Project key={name} imageUrl={imageUrl}>
-                           <div className="ml-1 flex w-full flex-col gap-3">
-                              <div className="flex w-full flex-row justify-between gap-3">
-                                 <Heading variant="h3" size="xl">
-                                    {name}
-                                 </Heading>
-
-                                 <div className="flex flex-row gap-3 px-3">
-                                    <Icons.GitHub className="dark:text-light-400 text-dark-300 my-auto aspect-[1.05] size-5 shrink-0" />
-                                    <Icons.Link className="dark:text-light-400 text-dark-300 my-auto aspect-[1.05] size-5 shrink-0" />
-                                 </div>
-                              </div>
-
-                              <Paragraph
-                                 variant="p"
-                                 size="base"
-                                 font="light"
-                                 className="mt-2 leading-6 max-md:max-w-full"
-                              >
-                                 {description}
-                              </Paragraph>
-
-                              <Paragraph
-                                 font="light"
-                                 variant="small"
-                                 className="text-primary-200 dark:text-primary-100 py-2 max-md:max-w-full"
-                              >
-                                 {stack.join(' - ')}
-                              </Paragraph>
-                           </div>
-                        </Project>
+                     {list.map((project) => (
+                        <ProjectComponent key={project.name} {...project} />
                      ))}
                   </div>
                ))}
