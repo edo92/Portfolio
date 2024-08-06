95d366eee13b1b440504db0293edee000cb85b5d
edo92 <edoter92@gmail.com>
2024-08-06 02:11:19 -0700

    update projects content

--- apps/client/src/app/content/intro.ts
+++ apps/client/src/app/content/intro.ts
@@ -1,10 +1,6 @@
 export const HERO_TITLE = "Hey I'm";
 export const NAME = 'Eduard Jacobs';
 export const ABOUT_TEXT = `
-I'm a software engineer based in Toronto, Canada and also a
-communication and journalism student. I enjoy creating things that live
-on the internet, whether that be websites, applications, or anything in
-between. I have been freelancing for a year now while studying at the
- university and I've manage to gain a decent amount of experience and
-valuable knowledge from all different kinds of fields throughout my
-projects/work.`;
+Backend Engineer with 5 years of experience in designing scalable and secure systems. Experienced in cloud
+technologies and distributed systems. Proven success in working with cross-functional teams and continuously
+improving skills and knowledge.  I enjoy creating things that live on the internet, whether that be websites, applications, or anything in between.`;
--- apps/client/src/app/content/projects.ts
+++ apps/client/src/app/content/projects.ts
@@ -11,9 +11,9 @@ export const projects = [
    {
       name: 'Full Stack Monorepo',
       description:
-         'The Full-Stack Monorepo v2 project uses Next.js for the frontend, NestJS for the backend, and AWS CDK for Infrastructure as Code (IaC), providing a cohesive, scalable setup for developing and deploying full-stack applications.',
+         'The Full-Stack Monorepo v2 project uses Next.js for the frontend, NestJS for the backend, and AWS CDK for Infrastructure as Code (IaC).',
       stack: ['Nextjs', 'NestJS', 'IaC', 'AWS CDK'],
-      link: 'https://www.example.com',
+      link: 'https://github.com/StellarScript/Full-Stack-Monorepo',
       imageUrl: '/static/projects/ecs_diagram.png',
       nav: 'View Project',
    },
@@ -24,29 +24,5 @@ export const projects = [
       stack: ['NextJS', 'Tailwind CSS', 'Typescript', 'Figma'],
       link: 'https://ejresume.com',
       imageUrl: '/static/projects/portfolio.png',
-   },
-   {
-      name: 'Project Four',
-      description: 'Social media platform with real-time chat and notifications',
-      stack: ['Angular', 'SCSS', 'WebSocket'],
-      link: 'https://www.yetanotherexample.com',
-      imageUrl:
-         'https://cdn.builder.io/api/v1/image/assets/TEMP/f2e97ab2969ac6aac36b09f470d6fece325a7c2323fcdc929d0eb7e72f629629?apiKey=e3660d42c58b4c259ebe598b9e99176a&width=2000',
-   },
-   {
-      name: 'Project Five',
-      description: 'Healthcare platform with appointment scheduling and video consultations',
-      stack: ['Ruby on Rails', 'PostgreSQL', 'Heroku'],
-      link: 'https://www.someexample.com',
-      imageUrl:
-         'https://cdn.builder.io/api/v1/image/assets/TEMP/f2e97ab2969ac6aac36b09f470d6fece325a7c2323fcdc929d0eb7e72f629629?apiKey=e3660d42c58b4c259ebe598b9e99176a&width=2000',
-   },
-   {
-      name: 'Project Six',
-      description: 'Educational platform with course management and interactive content',
-      stack: ['Django', 'React', 'Docker'],
-      link: 'https://www.educationexample.com',
-      imageUrl:
-         'https://cdn.builder.io/api/v1/image/assets/TEMP/f2e97ab2969ac6aac36b09f470d6fece325a7c2323fcdc929d0eb7e72f629629?apiKey=e3660d42c58b4c259ebe598b9e99176a&width=2000',
    },
 ];
