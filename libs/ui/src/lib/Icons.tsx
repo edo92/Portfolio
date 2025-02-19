2e92bb69ac37485525f835d8635e6f7339d20599
edo92 <edoter92@gmail.com>
2025-02-19 01:16:53 -0800

    update icons component

--- libs/ui/src/lib/Icons.tsx
+++ libs/ui/src/lib/Icons.tsx
@@ -110,4 +110,21 @@ export const Icons = {
       />
     </svg>
   ),
+
+  ArrowRight: (props: IconProps) => (
+    <svg
+      fill="none"
+      stroke="currentColor"
+      viewBox="0 0 24 24"
+      xmlns="http://www.w3.org/2000/svg"
+      {...props}
+    >
+      <path
+        strokeLinecap="round"
+        strokeLinejoin="round"
+        strokeWidth={2}
+        d="M14 5l7 7m0 0l-7 7m7-7H3"
+      />
+    </svg>
+  ),
 };
