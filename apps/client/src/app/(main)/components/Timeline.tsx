aa8c38963287187ed31a69a43647812098ba79be
edo92 <edoter92@gmail.com>
2025-02-19 00:16:38 -0800

    minor updates

--- apps/client/src/app/(main)/components/Timeline.tsx
+++ apps/client/src/app/(main)/components/Timeline.tsx
@@ -71,7 +71,7 @@ export default function Timeline() {
   return (
     <div
       ref={containerRef}
-      className="bg-background relative overflow-hidden py-20"
+      className="bg-background relative overflow-hidden py-10"
     >
       <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
         <motion.div
