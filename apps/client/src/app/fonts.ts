add166683146f7f635294ba16c1ff8be9783718d
edo92 <edoter92@gmail.com>
2025-02-23 10:23:04 -0800

    update client config - setup fonts and styles

---
+++ apps/client/src/app/fonts.ts
@@ -0,0 +1,49 @@
+import localFont from 'next/font/local';
+
+export const interBlack = localFont({
+  weight: '900',
+  style: 'normal',
+  display: 'swap',
+  src: '/fonts/Inter-Black.ttf',
+  variable: '--font-inter-black',
+});
+
+export const interBold = localFont({
+  weight: '700',
+  style: 'normal',
+  display: 'swap',
+  src: '/fonts/Inter-Bold.ttf',
+  variable: '--font-inter-bold',
+});
+
+export const interSemibold = localFont({
+  weight: '600',
+  style: 'normal',
+  display: 'swap',
+  src: '/fonts/Inter-SemiBold.ttf',
+  variable: '--font-inter-semibold',
+});
+
+export const interMedium = localFont({
+  weight: '500',
+  style: 'normal',
+  display: 'swap',
+  src: '/fonts/Inter-Medium.ttf',
+  variable: '--font-inter-medium',
+});
+
+export const interRegular = localFont({
+  weight: '400',
+  style: 'normal',
+  display: 'swap',
+  src: '/fonts/Inter-Regular.ttf',
+  variable: '--font-inter-regular',
+});
+
+export const interLight = localFont({
+  weight: '300',
+  style: 'normal',
+  display: 'swap',
+  src: '/fonts/Inter-Light.ttf',
+  variable: '--font-inter-light',
+});
