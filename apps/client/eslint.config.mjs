3a408936bb426dbd69f80026b07cfe7d9fad91c0
edo92 <edoter92@gmail.com>
2025-02-23 10:06:35 -0800

    update eslint | tailwind eslint plugin setup

--- apps/client/eslint.config.mjs
+++ apps/client/eslint.config.mjs
@@ -4,6 +4,8 @@ import js from '@eslint/js';
 import nx from '@nx/eslint-plugin';
 import { FlatCompat } from '@eslint/eslintrc';
 import { fixupConfigRules } from '@eslint/compat';
+import css from '@eslint/css';
+import { tailwindSyntax } from '@eslint/css/syntax';
 import baseConfig from '../../eslint.config.mjs';
 
 const compat = new FlatCompat({
@@ -23,15 +25,22 @@ const config = [
     ignores: ['.next/**/*'],
   },
   {
-    settings: {
-      tailwindcss: {
-        callees: ['classnames', 'clsx', 'cva', 'cn', 'className'],
-        config: './tailwind.config.js',
-      },
-    },
     rules: {
       'tailwindcss/classnames-order': 'warn',
       'tailwindcss/no-custom-classname': 'off',
+    },
+  },
+  {
+    files: ['**/*.css'],
+    plugins: {
+      css,
+    },
+    language: 'css/css',
+    languageOptions: {
+      customSyntax: tailwindSyntax,
+    },
+    rules: {
+      'css/no-empty-blocks': 'error',
     },
   },
 ];
