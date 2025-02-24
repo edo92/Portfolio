57d4bd444e98237d15f1a134dc72c9d853e77abc
edo92 <edoter92@gmail.com>
2025-02-24 06:10:15 -0800

    setup main app page & header component

---
+++ apps/client/src/app/(main)/components/Header.tsx
@@ -0,0 +1,48 @@
+import Link from 'next/link';
+import { Paragraph } from '@libs/ui';
+import { ThemeSwitch } from '../../components/ThemeSwitch';
+
+export const Header = () => (
+  <nav className="z-10 flex w-full items-center justify-between px-8 py-5">
+    <Link href="/">
+      <Paragraph variant="span" size="xl" weight="bold">
+        EJ
+      </Paragraph>
+    </Link>
+    <div className="flex items-center gap-6">
+      <Link href="/projects">
+        <Paragraph
+          variant="span"
+          weight="medium"
+          size="sm"
+          className="hover:text-accent-foreground/80"
+        >
+          Projects
+        </Paragraph>
+      </Link>
+      <Link href="/resume">
+        <Paragraph
+          variant="span"
+          weight="medium"
+          size="sm"
+          className="hover:text-accent-foreground/80"
+        >
+          Resume
+        </Paragraph>
+      </Link>
+      <Link href="/contact">
+        <Paragraph
+          variant="span"
+          weight="medium"
+          size="sm"
+          className="hover:text-accent-foreground/80"
+        >
+          Contact
+        </Paragraph>
+      </Link>
+      <div className="p-1">
+        <ThemeSwitch />
+      </div>
+    </div>
+  </nav>
+);
---
+++ apps/client/src/app/components/ThemeSwitch.tsx
@@ -0,0 +1,26 @@
+'use client';
+
+import { useTheme } from 'next-themes';
+import { cn, Icons } from '@libs/ui';
+
+interface ThemeSwitchProps {
+  className?: string;
+}
+
+export const ThemeSwitch: React.FC<ThemeSwitchProps> = ({ className }) => {
+  const { resolvedTheme, setTheme } = useTheme();
+
+  return (
+    <button
+      className={cn(
+        'group flex h-auto min-h-0 flex-col items-center p-0 ring-0',
+        className,
+      )}
+    >
+      <Icons.LightBulb
+        className="mt-0.5 aspect-square size-6 shrink-0 self-start transition-colors duration-200 hover:text-accent-foreground/80"
+        onClick={() => setTheme(resolvedTheme === 'light' ? 'dark' : 'light')}
+      />
+    </button>
+  );
+};
