afc19d185bed96c929198b720a3abae1ff01a518
edo92 <edoter92@gmail.com>
2025-02-25 16:04:42 -0800

    setup projects page & project hero component

---
+++ apps/client/src/app/(main)/project/page.tsx
@@ -0,0 +1,9 @@
+import { ProjectsHero } from './components/ProjectHero';
+
+export default function ProjectPage() {
+  return (
+    <main className="min-h-screen size-full">
+      <ProjectsHero />
+    </main>
+  );
+}
