e864d9e7f354383cf9cc0e3ccf6c4578a3279f91
edo92 <edoter92@gmail.com>
2025-03-08 05:42:40 -0800

    update social links urls

--- apps/client/src/app/content/social.tsx
+++ apps/client/src/app/content/social.tsx
@@ -3,21 +3,21 @@ import { Icons } from '@libs/ui';
 export const SOCIAL_LINKS = [
   {
     name: 'GitHub',
-    href: 'https://github.com',
+    href: 'https://github.com/edo92',
     icon: (
       <Icons.GitHub className="size-5 transition-all duration-200 group-hover:text-body/65" />
     ),
   },
   {
     name: 'LinkedIn',
-    href: 'https://linkedin.com',
+    href: 'https://www.linkedin.com/in/the-ej',
     icon: (
       <Icons.LinkedIn className="size-5 transition-all duration-200 group-hover:text-body/65" />
     ),
   },
   {
     name: 'Medium',
-    href: 'https://medium.com',
+    href: 'https://medium.com/@edoter92',
     icon: (
       <Icons.Medium className="size-5 transition-all duration-200 group-hover:text-body/65" />
     ),
