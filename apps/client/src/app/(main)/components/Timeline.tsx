dcac97cd8d70c7b431c61b10a009241a70edab53
edo92 <edoter92@gmail.com>
2025-03-09 22:54:53 -0700

    update timeline component

--- apps/client/src/app/(main)/components/Timeline.tsx
+++ apps/client/src/app/(main)/components/Timeline.tsx
@@ -20,21 +20,27 @@ interface TimelineEventProps {
   index: number;
 }
 
+const timelineEventVariants = {
+  hidden: { opacity: 0, y: 20 },
+  visible: (custom: number) => ({
+    opacity: 1,
+    y: 0,
+    transition: { duration: 0.8, delay: custom * 0.1 },
+  }),
+};
+
+const bulletAnimation = {
+  hidden: { scale: 0 },
+  visible: (custom: number) => ({
+    scale: 1,
+    transition: { duration: 0.5, delay: custom * 0.1 },
+  }),
+};
+
 const TimelineEvent: FC<TimelineEventProps> = ({ event, index }) => {
   const ref = useRef<HTMLDivElement>(null);
   const isInView = useInView(ref, { once: true, amount: 0.5 });
-
-  const containerAnimation = {
-    initial: { opacity: 0, y: 20 },
-    animate: isInView ? { opacity: 1, y: 0 } : { opacity: 0, y: 20 },
-    transition: { duration: 0.8, delay: index * 0.1 },
-  };
-
-  const bulletAnimation = {
-    initial: { scale: 0 },
-    animate: { scale: 1 },
-    transition: { duration: 0.5, delay: index * 0.1 },
-  };
+  const { year, title, description } = event;
 
   return (
     <motion.div
@@ -43,7 +49,10 @@ const TimelineEvent: FC<TimelineEventProps> = ({ event, index }) => {
         'flex w-full items-center justify-between',
         index % 2 === 0 && 'flex-row-reverse'
       )}
-      {...containerAnimation}
+      initial="hidden"
+      animate={isInView ? 'visible' : 'hidden'}
+      variants={timelineEventVariants}
+      custom={index}
     >
       <div className="w-5/12">
         <div className="transform-gpu cursor-pointer">
@@ -60,25 +69,28 @@ const TimelineEvent: FC<TimelineEventProps> = ({ event, index }) => {
                   weight="medium"
                   className="inline-block"
                 >
-                  {event.year}
+                  {year}
                 </Paragraph>
                 <Heading as="h3" weight="semibold" size="xl">
-                  {event.title}
+                  {title}
                 </Heading>
               </div>
               <Paragraph as="p" weight="medium" size="base">
-                {event.description}
+                {description}
               </Paragraph>
             </div>
           </motion.div>
         </div>
       </div>
 
-      {/* Timeline Bullet */}
+      {/* Timeline bullet */}
       <div className="z-10 flex items-center justify-center">
         <motion.div
           className="flex size-4 items-center justify-center rounded-full bg-card-foreground/80 p-3"
-          {...bulletAnimation}
+          initial="hidden"
+          animate={isInView ? 'visible' : 'hidden'}
+          variants={bulletAnimation}
+          custom={index}
         >
           <div className="size-2 rounded-full bg-background p-[5px]" />
         </motion.div>
@@ -97,6 +109,7 @@ export const Timeline: FC = () => {
     target: containerRef,
     offset: ['start end', 'end start'],
   });
+
   const scaleY = useSpring(scrollYProgress, {
     stiffness: 100,
     damping: 30,
@@ -106,7 +119,7 @@ export const Timeline: FC = () => {
   return (
     <Section ref={containerRef} secondary>
       <div className="container mx-auto flex max-w-screen-xl flex-col gap-12">
-        {/* Section header */}
+        {/* Section Header */}
         <motion.div
           className="flex flex-col items-center justify-center gap-6 text-center"
           initial={{ opacity: 0, y: 20 }}
@@ -121,15 +134,14 @@ export const Timeline: FC = () => {
           </Paragraph>
         </motion.div>
 
-        {/* Timeline events and vertical line */}
+        {/* Timeline events and vertical progress line */}
         <div className="relative space-y-12">
           <motion.div
             className={cn(
-              'absolute inset-y-0 left-1/2 w-0.5 -translate-x-1/2 bg-secondary-foreground/40 dark:bg-secondary/80'
+              'absolute inset-y-0 left-[49.95%] w-0.5 -translate-x-1/2 bg-secondary-foreground/40 dark:bg-secondary/80'
             )}
             style={{ scaleY, originY: 0, height: '105%', top: '0%' }}
           />
-
           {TIMELINE_EVENTS.map((event, index) => (
             <TimelineEvent key={event.year} event={event} index={index} />
           ))}
