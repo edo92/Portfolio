b6d07d9c9623099e32ebdc9db5c6cd0d7304a974
edo92 <edoter92@gmail.com>
2025-03-10 00:53:05 -0700

    update logo icon

--- apps/client/src/app/(main)/components/LogoIcon.tsx
+++ apps/client/src/app/(main)/components/LogoIcon.tsx
@@ -18,8 +18,8 @@ export const LogoIcon = () => {
     <>
       {isAnimating && (
         <svg
-          width="26"
-          height="26"
+          width="24"
+          height="24"
           viewBox="0 0 48 48"
           fill="none"
           xmlns="http://www.w3.org/2000/svg"
@@ -28,12 +28,12 @@ export const LogoIcon = () => {
           {/* E letter */}
           <motion.path
             d="M10 10 L10 38 M10 10 L25 10 M10 24 L22 24 M10 38 L25 38"
-            stroke="black"
-            strokeWidth="3"
+            strokeWidth="6"
             strokeLinecap="round"
             strokeLinejoin="round"
             initial={{ pathLength: 0, opacity: 0 }}
             animate={{ pathLength: 1, opacity: 1 }}
+            className="stroke-body"
             transition={{
               duration: 1.5,
               ease: 'easeInOut',
@@ -43,29 +43,29 @@ export const LogoIcon = () => {
 
           {/* Dot */}
           <motion.circle
-            cx="30"
+            cx="31.5"
             cy="24"
             r="1.6"
-            fill="black"
             initial={{ scale: 0, opacity: 0 }}
-            animate={{ scale: 1.2, opacity: 1 }}
+            animate={{ scale: 1.8, opacity: 1 }}
+            className='fill-body'
             transition={{
               duration: 0.3,
-              delay: 1.8,
+              delay: 1.2,
             }}
           />
 
           {/* J letter */}
           <motion.path
-            d="M40 10 L40 32 C40 36 37 38 34 38 L31 38"
-            stroke="black"
-            strokeWidth="3"
+            d="M42 10 L42 32 C42 36 39 38 36 38 L33 38"
+            strokeWidth="6"
             strokeLinecap="round"
             strokeLinejoin="round"
             initial={{ pathLength: 0, opacity: 0 }}
             animate={{ pathLength: 1, opacity: 1 }}
+            className="stroke-body"
             transition={{
-              duration: 1,
+              duration: 0.8,
               ease: 'easeInOut',
               delay: 0.4,
             }}
