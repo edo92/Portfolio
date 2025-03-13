ef4833b162f1fb11ca73722a1062d5df0812761e
edo92 <edoter92@gmail.com>
2025-03-13 08:38:32 -0700

    setup projects component

--- apps/client/src/app/(main)/(home)/page.tsx
+++ apps/client/src/app/(main)/(home)/page.tsx
@@ -1,11 +1,14 @@
 import { Hero } from './components/Hero';
 import { Timeline } from './components/Timeline';
+import { PROJECTS } from '../../content';
+import { ProjectsGrid } from '../../components/Projects';
 
 export default function HomePage() {
   return (
     <div className="size-full">
       <Hero />
       <Timeline />
+      <ProjectsGrid projects={PROJECTS} />
     </div>
   );
 }
