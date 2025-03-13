7be57b8d0610923d38fe3d6e111a1215e34c047c
edo92 <edoter92@gmail.com>
2025-03-13 03:24:09 -0700

    update eslint configs

--- apps/client/eslint.config.mjs
+++ apps/client/eslint.config.mjs
@@ -1,21 +1,39 @@
-import { FlatCompat } from '@eslint/eslintrc';
 import { dirname } from 'path';
 import { fileURLToPath } from 'url';
+
 import js from '@eslint/js';
-import { fixupConfigRules } from '@eslint/compat';
 import nx from '@nx/eslint-plugin';
+import { FlatCompat } from '@eslint/eslintrc';
+import { fixupConfigRules } from '@eslint/compat';
 import baseConfig from '../../eslint.config.mjs';
+
 const compat = new FlatCompat({
   baseDirectory: dirname(fileURLToPath(import.meta.url)),
   recommendedConfig: js.configs.recommended,
 });
 
-export default [
+const config = [
   ...fixupConfigRules(compat.extends('next')),
   ...fixupConfigRules(compat.extends('next/core-web-vitals')),
+  ...fixupConfigRules(compat.extends('plugin:tailwindcss/recommended')),
+
   ...baseConfig,
   ...nx.configs['flat/react-typescript'],
   {
     ignores: ['.next/**/*'],
   },
+  {
+    settings: {
+      tailwindcss: {
+        callees: ['classnames', 'clsx', 'cva', 'cn', 'className'],
+        config: './tailwind.config.js',
+      },
+    },
+    rules: {
+      'tailwindcss/classnames-order': 'warn',
+      'tailwindcss/no-custom-classname': 'off',
+    },
+  },
 ];
+
+export default config;
--- libs/ui/eslint.config.mjs
+++ libs/ui/eslint.config.mjs
@@ -1,12 +1,39 @@
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+import js from '@eslint/js';
 import nx from '@nx/eslint-plugin';
+import { FlatCompat } from '@eslint/eslintrc';
+import { fixupConfigRules } from '@eslint/compat';
 import baseConfig from '../../eslint.config.mjs';
 
-export default [
+const compat = new FlatCompat({
+  baseDirectory: dirname(fileURLToPath(import.meta.url)),
+  recommendedConfig: js.configs.recommended,
+});
+
+const config = [
+  ...fixupConfigRules(compat.extends('next')),
+  ...fixupConfigRules(compat.extends('next/core-web-vitals')),
+  ...fixupConfigRules(compat.extends('plugin:tailwindcss/recommended')),
+
   ...baseConfig,
-  ...nx.configs['flat/react'],
+  ...nx.configs['flat/react-typescript'],
   {
-    files: ['**/*.ts', '**/*.tsx', '**/*.js', '**/*.jsx'],
-    // Override or add rules here
-    rules: {},
+    ignores: ['.next/**/*'],
+  },
+  {
+    settings: {
+      tailwindcss: {
+        callees: ['classnames', 'clsx', 'cva', 'cn', 'className'],
+        config: './tailwind.config.js',
+      },
+    },
+    rules: {
+      'tailwindcss/classnames-order': 'warn',
+      'tailwindcss/no-custom-classname': 'off',
+    },
   },
 ];
+
+export default config;
