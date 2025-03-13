44f5c865a3ef30abdfa3f769f9ac71f01fd4cb67
edo92 <edoter92@gmail.com>
2025-03-13 02:03:57 -0700

    setup project structure

---
+++ libs/ui/src/lib/Typography/Paragraph.tsx
@@ -0,0 +1,51 @@
+import React, { forwardRef } from 'react';
+import { tv, type VariantProps } from 'tailwind-variants';
+import { cn } from '@/util';
+
+const paragraphVariants = tv({
+  base: 'text-foreground font-normal',
+  variants: {
+    variant: {
+      lead: 'text-balance text-lg font-normal',
+      body: 'text-base/6 font-normal',
+      caption: 'text-base font-normal',
+      link: 'text-base font-medium leading-6 transition-colors',
+      small: 'text-sm font-normal',
+    },
+    muted: {
+      true: 'text-muted-foreground',
+    },
+  },
+  defaultVariants: {
+    variant: 'lead',
+  },
+});
+
+export interface ParagraphProps
+  extends React.HTMLAttributes<HTMLParagraphElement>,
+    VariantProps<typeof paragraphVariants> {
+  as?: 'p' | 'small' | 'span';
+}
+
+const Paragraph = forwardRef<HTMLParagraphElement, ParagraphProps>(
+  (
+    { className, as: Component = 'p', muted = false, variant, ...props },
+    ref
+  ) => (
+    <Component
+      ref={ref}
+      className={cn(
+        paragraphVariants({
+          muted,
+          variant,
+          className,
+        })
+      )}
+      {...props}
+    />
+  )
+);
+
+Paragraph.displayName = 'Paragraph';
+
+export { Paragraph, paragraphVariants };
